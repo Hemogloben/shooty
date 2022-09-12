18dd84bda77c77023176541f2439937ed37ca1f9
Hemogloben <Hemogloben@gmail.com>
2022-09-11 22:31:24 -0400

    Adds bullet destruction and explosion on enemy hit

--- Bullet.gd
+++ Bullet.gd
@@ -2,9 +2,15 @@ extends Area2D
 
 export var speed = 750
 export var damage = 1
+var destroy = false
+onready var sprite = $AnimatedSprite
 
 func _physics_process(delta):
-	position += transform.x * speed * delta
+	if !destroy:
+		position += transform.x * speed * delta
+	else:
+		if !sprite.playing:
+			queue_free()
 
 func _on_VisibilityNotifier2D_screen_exited():
 	queue_free()
@@ -14,3 +20,6 @@ func _on_Bullet_area_entered(area):
 	if (area.is_in_group("mobs")):
 		print(area.name + " Hit by Bullet")
 		area.applyDamage(damage)
+		destroy = true
+		sprite.animation = "explosion"
+		sprite.play()
