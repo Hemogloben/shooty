c68549162f1e43ca888f458d65a2b7c04fd66619
MarisKeen <maris.keen@gmail.com>
2022-09-13 20:13:34 -0400

    Fixed reload issue
    Gun no longer shoots two bullets when firing last bullet in magazine.

--- shitty_gun.gd
+++ shitty_gun.gd
@@ -19,6 +19,11 @@ var bullet_pool = null
 
 #func _init():
 	#self.weapon_box_ui = get_node("/root/weapon_box_ui")
+func updateAmmoText():
+	if hud:
+		hud_weapon_box_name_ui.text = "Gun: " + Name
+		hud_weapon_box_ammo_count_ui.text = "Ammo: " + str(magazine_current)
+	weapon_box_ui.text = getAmmoPips()
 
 func getAmmoPips():
 	var pips = ""
@@ -33,8 +38,6 @@ func getAmmoPips():
 
 func _ready():
 	firerate.connect("timeout",self,"_on_cooldown_timeout")
-	weapon_box_ui.text = getAmmoPips()
-	print(magazine_current)
 	bullet_pool = get_node_or_null("/root/Node2D/BulletPool")
 	if (!bullet_pool):
 		bullet_pool = get_node("/root")
@@ -45,9 +48,8 @@ func _ready():
 			if weapon_box:
 				hud_weapon_box_name_ui = weapon_box.get_node_or_null("Panel/WeaponName")
 				hud_weapon_box_ammo_count_ui = weapon_box.get_node_or_null("Panel/AmmoCount")
-	if hud:
-		hud_weapon_box_name_ui.text = "Gun: " + Name
-		hud_weapon_box_ammo_count_ui.text = "Ammo: " + str(magazine_current)
+	updateAmmoText()
+
 
 func _on_cooldown_timeout():
 	can_shoot = true
@@ -60,14 +62,9 @@ func _process(_delta):
 		scale = Vector2(1, 1)
 	else:
 		scale = Vector2(1, -1)
-	get_input()
-	
-## get_input() exists because i read a post saying that you wouldnt want 
-## a reload funcion in the _process(_delta) function
-## it didnt seem to help. 
-func get_input():
 	if Input.is_action_pressed("action 1"):
 		shoot()
+
 		
 func shoot():
 	if can_shoot and magazine_current > 0:
@@ -77,26 +74,13 @@ func shoot():
 		firerate.start(between_shots)
 		can_shoot = false
 		magazine_current -= 1
-		print(magazine_current)
-		weapon_box_ui.text = getAmmoPips()
-		if hud:
-			hud_weapon_box_ammo_count_ui.text = "Ammo: " + str(magazine_current)
+		updateAmmoText()
 	elif magazine_current == 0:
 		reload()
-		##is this even the right way to do this? using an if/elif thing? 
-		##it seems like its saying "if you have bullets and can shoot, then shoot
-		##else, reload. so maybe it should just be if/else?
 
 func reload():
 	magazine_current = magazine_max
-	print("reloading")
-	print(magazine_current)
-	print(can_shoot)
-	can_shoot = true
-	print(can_shoot)
-	##this function seems to be running every frame action 1 is held
-	##so the output just reads "5 reloading 5 reloading" over and over
-	##it sets can_shoot to true, and magazine current to 5. im not sure why 
-	##func shoot() does not want to work after the first 5 bullets are fired
+	updateAmmoText()
+
 
 
