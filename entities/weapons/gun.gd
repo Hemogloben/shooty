775391aa16b58840e68d5c3fb28918c23def987a
Hemogloben <Hemogloben@gmail.com>
2022-09-23 21:42:38 -0400

    Create gun base class

---
+++ entities/weapons/gun.gd
@@ -0,0 +1,34 @@
+extends Node2D
+
+signal gun_properties_changed(props)
+
+export (Dictionary) var properties = {
+	name = "ShittyGun",
+	time_between_shots = .25,
+	reload_time = .75,
+	magazine_max = 10,
+	magazine_current = 5#properties.magazine_max
+}
+
+var bullet_pool = null
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	bullet_pool = get_node_or_null("/root/Node2D/BulletPool")
+	if (!bullet_pool):
+		bullet_pool = get_node("/root")
+
+func _process(_delta):
+	look_at(get_global_mouse_position())
+	var rot = wrapf(rotation_degrees, 0, 360)
+	if ((rot > 270) or (rot < 90)):
+		scale = Vector2(1, 1)
+	else:
+		scale = Vector2(1, -1)
+
+func force_emit_gun_signal():
+	emit_signal("gun_properties_changed", properties)
+
+func getProperties():
+	return properties
--- entities/weapons/shitty_gun.gd
+++ entities/weapons/shitty_gun.gd
@@ -1,22 +1,10 @@
-extends AnimatedSprite
+extends "gun.gd"
 
 export (PackedScene) var Bullet
 var can_shoot = true
 onready var shot_prevention_timer = Timer.new()
 
 onready var weapon_box_ui = $weapon_box_ui
-
-signal gun_properties_changed(props)
-
-export (Dictionary) var properties = {
-	name = "ShittyGun",
-	time_between_shots = .25,
-	reload_time = .75,
-	magazine_max = 10,
-	magazine_current = 5#properties.magazine_max
-}
-
-var bullet_pool = null
 
 func updateAmmoText():
 	weapon_box_ui.text = getAmmoPips(properties.magazine_current)
@@ -33,32 +21,20 @@ func getAmmoPips(ammo):
 	return pips
 
 func _ready():
+	._ready()
 	reload()
 	shot_prevention_timer.connect("timeout",self,"enable_shooting")
 	shot_prevention_timer.one_shot = true
 	add_child(shot_prevention_timer)
-	bullet_pool = get_node_or_null("/root/Node2D/BulletPool")
-	if (!bullet_pool):
-		bullet_pool = get_node("/root")
 	updateAmmoText()
 
-func force_emit_gun_signal():
-	emit_signal("gun_properties_changed", properties)
-
-func enable_shooting():
-	can_shoot = true
-
-
-func _process(_delta):
-	look_at(get_global_mouse_position())
-	var rot = wrapf(rotation_degrees, 0, 360)
-	if ((rot > 270) or (rot < 90)):
-		scale = Vector2(1, 1)
-	else:
-		scale = Vector2(1, -1)
+func _process(delta):
+	._process(delta)
 	if Input.is_action_pressed("action 1"):
 		shoot()
 
+func enable_shooting():
+	can_shoot = true
 
 func shoot():
 	if can_shoot and properties.magazine_current > 0:
@@ -78,6 +54,3 @@ func shoot():
 func reload():
 	properties.magazine_current = properties.magazine_max
 	updateAmmoText()
-
-func getProperties():
-	return properties
