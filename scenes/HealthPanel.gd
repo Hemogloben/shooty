8072a37208f7f3a58a47d2dc8bf5913c03de6191
Hemogloben <Hemogloben@gmail.com>
2022-09-25 19:25:16 -0400

    Slightly improved health bar

--- scenes/HealthPanel.gd
+++ scenes/HealthPanel.gd
@@ -6,7 +6,7 @@ export (PackedScene) var Heart
 
 
 func SetHealth(props):
-	hbox.set("custom_constants/separation", 26)
+	#hbox.set("custom_constants/separation", 26)
 	var max_health_diff = props.max_health - hbox.get_child_count()
 	if (max_health_diff > 0):
 		for _i in range(0, abs(max_health_diff)):
@@ -16,11 +16,13 @@ func SetHealth(props):
 #		for i in range(0, abs(max_health_diff)):
 	var hearts = hbox.get_children()
 	for i in range(0, hbox.get_child_count()):
-		#hbox.fit_child_in_rect(hearts[i], Rect2(Vector2(), hbox.rect_size))
+		#hbox.fit_child_in_rect(hearts[i], Rect2(Vector2(), hbox.rect_size / props.max_health))
 		if i < props.health_current:
-			hearts[i].get_child(0).frame = 0
+			hearts[i].get_node("Full").show()
+			hearts[i].get_node("Empty").hide()
 		else:
-			hearts[i].get_child(0).frame = 1	
+			hearts[i].get_node("Full").hide()
+			hearts[i].get_node("Empty").show()
 	print("Num Children: " + str(hbox.get_child_count()))
 		
 		
